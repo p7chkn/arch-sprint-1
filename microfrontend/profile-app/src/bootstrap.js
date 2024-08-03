595e7118bee6c87ce62b815760bde818d0e556cc
chuikin.p <chuikin.p@selectel.com>
2024-08-03 22:09:57 +0300

    add first iteration on task 1

---
+++ microfrontend/profile-app/src/bootstrap.js
@@ -0,0 +1,12 @@
+// button-app/src/index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "./components/Button";
+import Profile from "./components/Profile";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Profile />
+  </React.StrictMode>,
+  document.getElementById("root"),
+);
---
+++ microfrontend/profile-app/src/components/Button.js
@@ -0,0 +1,11 @@
+// button-app/src/Button.js
+
+import React from "react";
+
+function Button() {
+  return (
+    <button onClick={() => console.log("Button clicked!")}>Click me!</button>
+  );
+}
+
+export default Button;
